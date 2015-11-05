efe8840e2bb597ca6e23d2a454ff666c7e5926e4
Max Oldham <maximusoldham@gmail.com>
2015-11-05 10:43:06 -0800

    initial commit, added new gemfiles

--- Gemfile
+++ Gemfile
@@ -38,7 +38,8 @@ gem 'bootstrap_form'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'puma'
+gem 'rails_12factor', group: :production
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -51,4 +52,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+
+ruby '2.2.3'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,16 @@
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require_tree .
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Poiret+One::latin', 'Josefin+Sans::latin', 'Pacifico::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
